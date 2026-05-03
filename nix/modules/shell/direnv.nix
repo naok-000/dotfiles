adbf0dd6df448a6d81a8165043af148d0da1edac
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-05-03 17:33:54 +0900

    feat: Add tests to direnv and fix nvim pattern syntax

--- nix/modules/shell/direnv.nix
+++ nix/modules/shell/direnv.nix
@@ -1,6 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   programs.direnv = {
     enable = true;
+    package = pkgs.direnv.overrideAttrs {
+      checkPhase = ''
+        runHook preCheck
+        make test-go test-bash test-fish
+        runHook postCheck
+      '';
+    };
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
