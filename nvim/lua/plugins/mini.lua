5f259ac26e346de3970959336ff4c4df6b5895b7
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-05-16 11:52:18 +0900

    feat: Remove leader key from mini.surround mappings

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -4,12 +4,12 @@ return {
 	config = function()
 		require("mini.surround").setup({
 			mappings = {
-				add = "<leader>sa", -- Add surrounding in Normal and Visual modes
-				delete = "<leader>sd", -- Delete surrounding
-				find = "<leader>sf", -- Find surrounding (to the right)
-				find_left = "<leader>sF", -- Find surrounding (to the left)
-				highlight = "<leader>sh", -- Highlight surrounding
-				replace = "<leader>sr", -- Replace surrounding
+				add = "sa", -- Add surrounding in Normal and Visual modes
+				delete = "sd", -- Delete surrounding
+				find = "sf", -- Find surrounding (to the right)
+				find_left = "sF", -- Find surrounding (to the left)
+				highlight = "sh", -- Highlight surrounding
+				replace = "sr", -- Replace surrounding
 				suffix_last = "l", -- Suffix to search with "prev" method
 				suffix_next = "n", -- Suffix to search with "next" method
 			},
