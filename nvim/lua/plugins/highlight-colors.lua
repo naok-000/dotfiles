885cfe03ecd484d3ae3a0203952a99fa6192bd44
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-05-23 20:44:41 +0900

    feat:

--- nvim/lua/plugins/highlight-colors.lua
+++ nvim/lua/plugins/highlight-colors.lua
@@ -1,6 +1,16 @@
 return {
   "brenoprata10/nvim-highlight-colors",
   config = function()
-    require("nvim-highlight-colors").setup({})
+    require("nvim-highlight-colors").setup({
+      exclude_buftypes = {
+        "help",
+        "nofile",
+        "prompt",
+        "quickfix",
+      },
+      exclude_buffer = function(bufnr)
+        return not vim.api.nvim_buf_is_loaded(bufnr)
+      end,
+    })
   end,
 }
