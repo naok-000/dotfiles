96f89a51f666271700ed7390cc0f652376f92aa8
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-05-29 11:59:54 +0900

    fix: Build markdown formatter from source

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,6 +1,11 @@
 {markdown-formatter-ja}: final: prev: {
   czg = final.callPackage ../pkgs/czg.nix {};
   libtexprintf = final.callPackage ../pkgs/libtexprintf.nix {};
-  markdown-formatter-ja = markdown-formatter-ja.packages.${final.stdenv.hostPlatform.system}.default;
+  markdown-formatter-ja = final.rustPlatform.buildRustPackage {
+    pname = "markdown-formatter-ja";
+    version = "0.1.0";
+    src = markdown-formatter-ja;
+    cargoHash = "sha256-UwVk6Y/1ewdzo7ehJCKKW8sv4TX7orQdTtQCRR4EjRs=";
+  };
   mo = final.callPackage ../pkgs/mo.nix {};
 }
