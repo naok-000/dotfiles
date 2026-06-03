42a015d9dc5cc468a5aefcd503999f568c01de07
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-06-03 12:50:36 +0900

    feat: Update Doom Emacs config and enable more modules

--- doom/config.el
+++ doom/config.el
@@ -28,11 +28,15 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+ 
+(setq doom-font (font-spec :family "HackGen35 Console NF" :size 16 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Hiragino Sans" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;;(setq doom-theme 'doom-one)
+(setq doom-theme 'modus-operandi)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -40,7 +44,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/workspace/ghq/github.com/naok-000/life/org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
